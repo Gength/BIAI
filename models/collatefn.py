37ed3ddbb834cb7e7ebdca05936a26f9cd6f48f8
Gength <gengtianhao@outlook.com>
2025-06-14 19:58:08 +0200

    add adjustable sample option

--- models/collatefn.py
+++ models/collatefn.py
@@ -3,10 +3,11 @@ import torch
 import random
 from models.dataset import BERTMLMDataset, BERTANPDataset, TaskDataset
 class MLMCollateFn:
-    def __init__(self, tokenizer, seq_len=128, train=True):
+    def __init__(self, tokenizer, seq_len=128, train=True, samples_per_batch=20):
         self.tokenizer = tokenizer
         self.seq_len = seq_len
         self.train = train
+        self.samples_per_batch = samples_per_batch
 
     def __call__(self, batches):
         ids_output = []
@@ -16,6 +17,12 @@ class MLMCollateFn:
             mlm_dataset = BERTMLMDataset(instruction_blocks, self.tokenizer, max_len=self.seq_len, train=self.train)
             # Only take one sample per graph to avoid data imbalance
             if len(mlm_dataset) > 0:
+                if len(mlm_dataset) < self.samples_per_batch:
+                    # If dataset is smaller than samples_per_batch, take all samples
+                    idx = range(len(mlm_dataset))
+                else:
+                    # Randomly select samples from the dataset
+                    idx = random.sample(range(len(mlm_dataset)), self.samples_per_batch)
                 idx = random.randint(0, len(mlm_dataset)-1)
                 ids, labels = mlm_dataset[idx]
                 ids_output.append(ids)
@@ -34,9 +41,10 @@ class MLMCollateFn:
         }
 
 class ANPCollateFn:
-    def __init__(self, tokenizer, seq_len=128):
+    def __init__(self, tokenizer, seq_len=128, samples_per_batch=20):
         self.tokenizer = tokenizer
         self.seq_len = seq_len
+        self.samples_per_batch = samples_per_batch
 
     def __call__(self, batches):
         ids_a_output = []
@@ -48,7 +56,12 @@ class ANPCollateFn:
             anp_dataset = BERTANPDataset(instruction_blocks, self.tokenizer, adj, max_len=self.seq_len)
             # Only take one sample per graph to avoid data imbalance
             if len(anp_dataset) > 0:
-                idx = random.randint(0, len(anp_dataset)-1)
+                if len(anp_dataset) < self.samples_per_batch:
+                    # If dataset is smaller than samples_per_batch, take all samples
+                    idx = range(len(anp_dataset))
+                else:
+                    # Randomly select samples from the dataset
+                    idx = random.sample(range(len(anp_dataset)), self.samples_per_batch)
                 ids_a, ids_b, label = anp_dataset[idx]
                 ids_a_output.append(ids_a)
                 ids_b_output.append(ids_b)
